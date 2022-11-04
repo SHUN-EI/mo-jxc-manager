baedd8bdf6a73d31ffdd426c87935291a4749d96
moweiquan <295597613@qq.com>
2022-11-04 23:08:05 +0800

    进销存系统-用户退出

--- mo-jxc-admin/src/main/java/com/mo/controller/MainController.java
+++ mo-jxc-admin/src/main/java/com/mo/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.mo.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by mo on 2022/10/31
@@ -13,31 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MainController {
 
-    /**
-     * 系统欢迎页
-     *
-     * @return
-     */
+    @ApiOperation("用户退出")
+    @RequestMapping("signout")
+    public String signout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:index";
+    }
+
+    @ApiOperation("系统欢迎页")
     @GetMapping("welcome")
     public String welcome() {
         return "welcome";
     }
 
-    /**
-     * 系统主页面
-     *
-     * @return
-     */
+    @ApiOperation("系统主页面")
     @GetMapping("main")
     public String main() {
         return "main";
     }
 
-    /**
-     * 系统登录页
-     *
-     * @return
-     */
+    @ApiOperation("系统登录页")
     @GetMapping("index")
     public String index() {
         return "index";
